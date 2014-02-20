684c8d3431433dd0251c0db3c73b89b1fe2d544d
Nickolas Lanasa <nickolas@olympusat.com>
2014-02-20 11:55:53 -0500

    Initial Commit

---
+++ Derive/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Derive
+//
+//  Created by Nick Lanasa on 2/20/14.
+//  Copyright (c) 2014 Nytek Productions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "DAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([DAppDelegate class]));
+    }
+}
---
+++ DeriveTests/DeriveTests.m
@@ -0,0 +1,34 @@
+//
+//  DeriveTests.m
+//  DeriveTests
+//
+//  Created by Nick Lanasa on 2/20/14.
+//  Copyright (c) 2014 Nytek Productions. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface DeriveTests : XCTestCase
+
+@end
+
+@implementation DeriveTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
